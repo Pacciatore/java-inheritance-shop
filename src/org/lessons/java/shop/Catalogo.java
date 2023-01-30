9ea81b6466cbda31c63373bb9c7d1f0609712061
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-30 19:18:05 +0100

    definizione metodo per inserimento di televisore

--- src/org/lessons/java/shop/Catalogo.java
+++ src/org/lessons/java/shop/Catalogo.java
@@ -45,7 +45,7 @@ public class Catalogo {
 				break;
 
 			case 2:
-				System.out.println("Televisore");
+				televisore(s);
 				break;
 
 			case 3:
@@ -100,6 +100,56 @@ public class Catalogo {
 		System.out.println("IVA: " + smartphone.getVat() + "%");
 		System.out.println("Codice IMEI: " + smartphone.getImei());
 		System.out.println("Capacità di memoria: " + smartphone.getMemoryCapacity() + "GB");
+
+		System.out.println("------------------------------------------------");
+
+	}
+
+	public static void televisore(Scanner s) {
+
+		System.out.println("------------------------------------------------");
+		System.out.println("Registrazione Televisore");
+		System.out.println();
+
+		System.out.println("Inserire nome prodotto: ");
+		String productName = s.nextLine();
+
+		System.out.println("Inserire marchio: ");
+		String productBrand = s.nextLine();
+
+		System.out.println("Inserire prezzo: ");
+		float productPrice = Float.parseFloat(s.nextLine());
+
+		System.out.println("Inserire percentuale IVA: ");
+		int productVat = Integer.parseInt(s.nextLine());
+
+		System.out.println("Inserire grandezza in pollici: ");
+		float productInches = Float.parseFloat(s.nextLine());
+
+		System.out.println("Smart TV? (S/N): ");
+		String smartTvChoice = s.nextLine();
+		boolean productSmartTv = false;
+		if (smartTvChoice.toLowerCase().equals("s"))
+			productSmartTv = true;
+		else
+			productSmartTv = false;
+
+		Televisore tv = new Televisore(productName, productBrand, productPrice, productVat, productInches,
+				productSmartTv);
+
+		System.out.println("------------------------------------------------");
+
+		System.out.println("Riepilogo dati inseriti: ");
+
+		System.out.println("Nome: " + tv.getName());
+		System.out.println("Marchio: " + tv.getBrand());
+		System.out.println("Prezzo: " + tv.getPriceFormatted() + "€");
+		System.out.println("IVA: " + tv.getVat() + "%");
+		System.out.println("Codice IMEI: " + tv.getInches());
+		if (tv.isSmartTv())
+			System.out.println("Smart TV");
+		else
+			System.out.println("Non Smart TV");
 
 		System.out.println("------------------------------------------------");
 
